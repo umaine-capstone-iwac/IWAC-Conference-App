0c85877d0c2fe0d3b35a12d64be9241f72daa3f2
Brett Palmer <brett.palmer@maine.edu>
2025-11-21 11:51:00 -0500

    Added messages icon to tab navigator, and dummy Messages screen.

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: 'Meessages',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="message.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
